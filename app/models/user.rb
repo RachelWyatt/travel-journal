38d6a3033643695913a2edaa9fe088c74b923e5c
Rachel Wyatt <rw126@evansville.edu>
2019-03-24 15:32:02 -0400

    add #sort_by_most_recent to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     has_many :trips 
     has_many :trip_entries, through: :trips
+
+    def sort_by_most_recent
+        entries = self.trip_entries.all 
+        entries.order("created_at DESC")
+    end
 end
