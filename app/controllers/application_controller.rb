5eea69ec04d7fb1658ab892f38b6ce420c33f96b
Rachel Wyatt <rw126@evansville.edu>
2019-03-25 07:38:19 -0400

    add #logged_in? and stub out #current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    private 
+
+    def current_user
+        #@current_user ||= User.find_by(id: session[:user_id])
+        @current_user = User.first #for testing purposes
+    end
+
+    def logged_in?
+        !!current_user 
+    end
 end
