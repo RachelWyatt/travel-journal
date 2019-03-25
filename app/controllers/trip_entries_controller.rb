880c211d67f022e4d7d4d5a6b4b616697eb5ef6c
Rachel Wyatt <rw126@evansville.edu>
2019-03-25 07:23:19 -0400

    generate trip entries controller

---
+++ app/controllers/trip_entries_controller.rb
@@ -0,0 +1,20 @@
+class TripEntriesController < ApplicationController
+
+    def index 
+    end 
+
+    def new 
+    end
+
+    def create 
+    end 
+
+    def show 
+    end
+
+    def edit 
+    end 
+
+    def destroy
+    end
+end
---
+++ app/helpers/trip_entries_helper.rb
@@ -0,0 +1,2 @@
+module TripEntriesHelper
+end
