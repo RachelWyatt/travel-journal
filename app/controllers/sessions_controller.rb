c80d68b4f0748b795d5a8ea4e36b4e5ecbbe64e0
Rachel Wyatt <rw126@evansville.edu>
2019-03-27 07:37:40 -0400

    add log in functionality, error message if failed to authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,19 @@
 class SessionsController < ApplicationController
 
+    def new 
+    end 
+
+    def create 
+        @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id 
+            redirect_to trips_path            
+        else 
+            flash[:message] = "Something went wrong"
+            redirect_to login_path
+        end
+    end
+
     def destroy
         session.clear 
         redirect_to root_path
