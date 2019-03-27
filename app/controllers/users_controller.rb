b632abe0eb1858841d7650e465ea71fce05cea20
Rachel Wyatt <rw126@evansville.edu>
2019-03-27 07:15:21 -0400

    add #create for users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save 
+            session[:user_id] = @user.id 
+            flash[:message] = "Successfully signed up!"
+            redirect_to trips_path
+        else 
+            render :new 
+        end
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end
