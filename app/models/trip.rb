b41710296092f824cd7d7b39e9621148c17bb8cb
Rachel Wyatt <rw126@evansville.edu>
2019-03-31 09:21:54 -0400

    refactor scope method in trip.rb

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,8 +2,6 @@ class Trip < ApplicationRecord
     belongs_to :user 
     has_many :trip_entries
     has_many :locations, through: :trip_entries
+    scope :descending_order, -> {order("created_at DESC")}
 
-    def self.descending_order 
-        order("created_at DESC")
-    end
 end
