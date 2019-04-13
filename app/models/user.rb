5a567237e393df221c26500c297f5d600c5307c7
Rachel Wyatt <rw126@evansville.edu>
2019-04-13 11:08:25 -0400

    refactor locations list into helper method user_locations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
     has_many :trip_entries, through: :trips
     validates :email, presence:true, uniqueness: true
 
+    def user_locations 
+        loc_array = []
+        trips.each do |t|
+            t.locations.each do |l|
+                loc_array << l.name
+            end
+        end
+        loc_array.uniq!
+    end
+
 end
